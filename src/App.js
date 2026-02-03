d6370e2b04947ea11acdbc15a42dcff5ab007aa5
Nicholas <nickdadams24@gmail.com>
2026-02-02 20:13:22 -0500

    Hello World UI + Dockerfile + README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="card">
+        <h1>Hello World </h1>
+        <p>My name is Nicholas Adams. This is my first React + Docker assignment.</p>
+      </div>
     </div>
   );
 }
 
-export default App;
+
+
